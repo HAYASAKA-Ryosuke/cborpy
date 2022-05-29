7b4b5cdf0f730b16713cc6a8618f899ff3a40747
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2022-05-29 15:15:50 +0900

    変換結果がずれるので修正

--- convert_byte.py
+++ convert_byte.py
@@ -1,13 +1,20 @@
-
+import struct
 
 def encode_byte(major: int, length: int):
+    """
+    length < 24: 引数を付加情報に加える
+    24 <= length < 2**8: 引数 + 24を付加情報に加える
+    2**8 <= length < 2**16: 引数 + 25を付加情報に加える
+    2**16 <= length < 2**32: 引数 + 26を付加情報に加える
+    2**32 <= length < 2**64: 引数 + 27を付加情報に加える
+    """
     if length < 24:
-        return int.to_bytes((major << 5) | length, 1, 'big')
+        return struct.pack('>B', (major << 5) | length)
     elif length < 2**8:
-        return int.to_bytes((major << 5) + 24, 1, 'big') + int.to_bytes(length, 1, 'big')
+        return struct.pack('>BB', (major << 5) | 24, length)
     elif length < 2**16:
-        return int.to_bytes((major << 5) + 25, 1, 'big') + int.to_bytes(length, 2, 'big')
+        return struct.pack('>BH', (major << 5) | 25, length)
     elif length < 2**32:
-        return int.to_bytes((major << 5) + 26, 1, 'big') + int.to_bytes(length, 4, 'big')
+        return struct.pack('>BI', (major << 5) | 26, length)
     elif length < 2**64:
-        return int.to_bytes((major << 5) + 27, 1, 'big') + int.to_bytes(length, 6, 'big')
+        return struct.pack('>BQ', (major << 5) | 27, length)
