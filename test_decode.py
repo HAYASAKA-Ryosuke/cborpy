9e75c7d433bc267388c68111fbc03586f0941d7f
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2022-06-27 03:13:57 +0900

    テストを修正

--- test_decode.py
+++ test_decode.py
@@ -4,13 +4,13 @@ from decode import decode
 
 class TestDecode(unittest.TestCase):
     def test_decode(self):
-        self.assertEqual(decode(b'\x00'), 0)
-        self.assertEqual(decode(b'\x01'), 1)
-        self.assertEqual(decode(b'\x0a'), 10)
-        self.assertEqual(decode(b'\x17'), 23)
-        self.assertEqual(decode(b'\x18\x18'), 24)
-        self.assertEqual(decode(b'\x18\x19'), 25)
-        self.assertEqual(decode(b'\x8A\x05\x25\x14\x01\x02\x03\x04\x05\x06\x07'), [5, -6, 20, 1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(decode('00'), 0)
+        self.assertEqual(decode('01'), 1)
+        self.assertEqual(decode('0a'), 10)
+        self.assertEqual(decode('17'), 23)
+        self.assertEqual(decode('1818'), 24)
+        self.assertEqual(decode('1819'), 25)
+        self.assertEqual(decode('8A05251401020304050607'), [5, -6, 20, 1, 2, 3, 4, 5, 6, 7])
 
 if __name__ == "__main__":
     unittest.main()
